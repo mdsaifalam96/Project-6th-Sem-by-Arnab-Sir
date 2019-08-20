7f54a1387981699ad0c37d07c250adf8d36141c6
MD SAIF ALAM <38750964+mdsaifalam96@users.noreply.github.com>
2019-08-20 22:57:16 +0530

    Update Tagline.js

--- Resturent-App-For-Berger/src/Components/Layouts/Tagline.js
+++ Resturent-App-For-Berger/src/Components/Layouts/Tagline.js
@@ -5,6 +5,7 @@ const Tagline = () => {
         <section id="lock">
             <h2>SERVING MORE THAN JUST BURGERS SINCE 2019</h2>
             <p>Check out our opening hours and location address below.</p>
+            <p>10:00am to 05:00 pm University Carmichael Hall.</p>
         </section>
     );
 };
